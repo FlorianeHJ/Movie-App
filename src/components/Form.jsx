d0641305f5a884f2508e3c12e29ded9f1badec7b
Floriane HJ <fjulia.dev@gmail.com>
2024-09-19 16:27:10 +0200

    Ajout bdd movies+card

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,15 @@
 import axios from "axios";
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import Card from "./Card";
 
 
 const Form = () => {
 
+    const [moviesData, setMoviesData] = useState([])
+
     useEffect(() => {
-        axios.get(`https://api.themoviedb.org/3/search/movie?api_key=09ba41025f3fca8724a8283ba52f321f&query=code&language=fr-FR`).then((res) => console.log(res))
+        axios.get(`https://api.themoviedb.org/3/search/movie?api_key=09ba41025f3fca8724a8283ba52f321f&query=code&language=fr-FR`).then((res) => setMoviesData(res.data.results       
+        ))
     }, [])
 
     return (
@@ -24,7 +28,13 @@ const Form = () => {
                     </div>
                 </div>
             </div>
-            <div className="result">RESULTAT</div>
+            <div className="result">
+                {
+                    moviesData.slice(0,12).map((movie) => (
+                        <Card movie={movie} key={movie.id}/>
+                    ))
+                }
+            </div>
         </div>
     );
 };
